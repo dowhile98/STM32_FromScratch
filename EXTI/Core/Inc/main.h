8a0350667b58943781894665e321781bd8a9e7d5
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2023-03-16 09:22:15 -0500

    update

---
+++ EXTI/Core/Inc/main.h
@@ -0,0 +1,19 @@
+/*
+ * main.h
+ *
+ *  Created on: 1 feb. 2023
+ *      Author: jeffr
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "defines.h"
+/*Defines -------------------------------------------*/
+#define SW				A, 0
+#define LED1			D, 12
+#define LED2			D, 13
+#define LED3			D, 14
+#define LED4			D, 15
+
+#endif /* INC_MAIN_H_ */
