1dc060f45270d3ad2c0b561bb6978241fed56286
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2023-10-02 22:02:12 -0500

    udpdate

---
+++ SETIEMBRE-2023/SysTick_Polling/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/core_cm0plus.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/core_cm0plus.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_version.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/TRABAJO/UMAKER/CURSOS/ARM\ -\ NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h:
---
+++ SETIEMBRE-2023/SysTick_Polling/Debug/Src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32c0xx.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32c0xx.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32c0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DSTM32C031xx -DNUCLEO_C031C6 -DSTM32 -DSTM32C0 -DSTM32C031C6Tx -c -I../Inc -I"D:/TRABAJO/UMAKER/CURSOS/ARM - NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Include" -I"D:/TRABAJO/UMAKER/CURSOS/ARM - NUCLEO/STM32_FromScratch/SETIEMBRE-2023/SysTick_Polling/Drivers/CMSIS/Device/ST/STM32C0xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32c0xx.cyclo ./Src/system_stm32c0xx.d ./Src/system_stm32c0xx.o ./Src/system_stm32c0xx.su
+
+.PHONY: clean-Src
+
