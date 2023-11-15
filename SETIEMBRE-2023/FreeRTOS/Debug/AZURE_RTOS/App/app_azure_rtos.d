92cd0325888fc531010d23ad115c8d086fef8c7e
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2023-11-14 21:04:16 -0500

    udpdate

---
+++ SETIEMBRE-2023/FreeRTOS/Debug/AZURE_RTOS/App/app_azure_rtos.d
@@ -0,0 +1,12 @@
+AZURE_RTOS/App/app_azure_rtos.o: ../AZURE_RTOS/App/app_azure_rtos.c \
+ ../AZURE_RTOS/App/app_azure_rtos.h \
+ ../AZURE_RTOS/App/app_azure_rtos_config.h ../Core/Inc/app_threadx.h \
+ ../Middlewares/ST/threadx/common/inc/tx_api.h \
+ ../Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h \
+ ../Core/Inc/tx_user.h
+../AZURE_RTOS/App/app_azure_rtos.h:
+../AZURE_RTOS/App/app_azure_rtos_config.h:
+../Core/Inc/app_threadx.h:
+../Middlewares/ST/threadx/common/inc/tx_api.h:
+../Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h:
+../Core/Inc/tx_user.h:
